c539195f7a1ef8d3acb38d1e19597bf843bad307
LNR14 <lnrabc@gmail.com>
2020-03-12 13:19:50 -0500

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/PostManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
-    err => {
-        if(!err)
-            console.log("Connected successfully");
-        else
-            console.log("Error while connecting mongodb " + JSON.stringify(err,undefined,2))
-})
